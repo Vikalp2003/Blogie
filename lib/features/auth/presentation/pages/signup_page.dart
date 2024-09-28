d552325aa67a7c8e53aaea9460309c941aba1938
Vikalp2003 <vikalpshinde25@gmail.com>
2024-09-29 00:29:47 +0530

    first commit

---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,103 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blog_app/widgets/auth_field.dart';
+import 'package:blog_app/widgets/auth_gradient_button.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (context) => const SignUpPage());
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // formKey.currentState!.validate();
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: formKey,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  AuthField(
+                    hintText: 'Name',
+                    controller: nameController,
+                  ),
+                  const SizedBox(height: 15),
+                  AuthField(
+                    hintText: 'Email',
+                    controller: emailController,
+                  ),
+                  const SizedBox(height: 15),
+                  AuthField(
+                      hintText: 'Password',
+                      controller: passwordController,
+                      password: true),
+                  const SizedBox(height: 15),
+                  const AuthGradientButton(
+                    buttonText: "Sign up",
+                  ),
+                  const SizedBox(height: 15),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        LoginPage.route(),
+                      );
+                    },
+                    child: RichText(
+                      text: TextSpan(
+                          text: "Already have an account? ",
+                          style: Theme.of(context).textTheme.titleMedium,
+                          children: [
+                            TextSpan(
+                                text: "Sign in",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium
+                                    ?.copyWith(
+                                      color: AppPallete.gradient2,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                          ]),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
